30a0abafbaefd06e3ebef1fa6cdee9efbee60ebb
mikehe123 <71740368+mikehe123@users.noreply.github.com>
2022-10-21 15:24:47 -0400

    Revert "Added the carousel component"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import styles from "@styles/Home.module.css";
 import Listing from "@components/listing/Listing";
-import CarrotCell from "@components/carousel/Carousel";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +8,6 @@ const Home: NextPage = () => {
       <div className={styles.bannerBoard}>
         <div className={styles.text}>Component Example: Listing</div>
       </div>
-      <CarrotCell listingTitle="Recently Viewed" />
       <Listing listingTitle="Popular" />
     </div>
   );
